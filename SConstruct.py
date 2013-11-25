fb4865758c5be63fa7779c139261d5b96c1a4378
vmlaker <velimir.mlaker@gmail.com>
2013-11-25 09:48:13 -0800

    Add optional debug build.

--- SConstruct.py
+++ SConstruct.py
@@ -2,6 +2,9 @@
 
 import os 
 
+# Retrieve the debug flag, if set.
+debug = bool(int(ARGUMENTS.get('debug', False)))
+
 # Names of .cpp classes.
 names = [
     'Averager',
@@ -17,6 +20,7 @@ env = Environment(
     CPPPATH='include',
     CXXFLAGS='-std=c++11',
 )
+if debug: env.Append(CXXFLAGS = ' -g')
 env.Library(target=target, source=sources)
 
 # Add header-only classes.
@@ -39,6 +43,7 @@ env = Environment(
     LIBS=libs,
     CXXFLAGS='-std=c++11',
 ) 
+if debug: env.Append(CXXFLAGS = ' -g')
 env.Program(
     target=os.path.join('bin', 'run_tests'), 
     source=sources,
