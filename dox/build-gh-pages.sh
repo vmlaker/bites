cbe05efcfd771e9cebb5d9520430c7238c72cc8a
vmlaker <velimir.mlaker@gmail.com>
2013-12-06 17:25:40 -0800

    Fix typo: missing "sh" before script.

--- dox/build-gh-pages.sh
+++ dox/build-gh-pages.sh
@@ -5,7 +5,7 @@ scons -j8 .
 bin/run_tests --report_level=short
 scons -c .
 
-dox/build-dox.sh
+sh dox/build-dox.sh
 git checkout gh-pages
 find . -maxdepth 1 ! -name 'html' ! -name '.git' | xargs rm -rf
 mv html/* .
