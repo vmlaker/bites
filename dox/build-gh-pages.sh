7030d2dab507a83f2d9242a8089f306810dec89b
vmlaker <velimir.mlaker@gmail.com>
2013-12-06 17:29:49 -0800

    Fix build script to correctly insert version number.

--- dox/build-gh-pages.sh
+++ dox/build-gh-pages.sh
@@ -1,11 +1,12 @@
 # Script to build gh-pages.
 
 git pull
+scons -c .
 scons -j8 .
 bin/run_tests --report_level=short
+sh dox/build-dox.sh
 scons -c .
 
-sh dox/build-dox.sh
 git checkout gh-pages
 find . -maxdepth 1 ! -name 'html' ! -name '.git' | xargs rm -rf
 mv html/* .
